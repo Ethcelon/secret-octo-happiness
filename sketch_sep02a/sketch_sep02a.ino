8128d26c3ed250f00dd70f9eb69f27ba45c9b7e6
Ethcelon <om.bhallamudi@gmail.com>
2013-09-04 19:21:12 +0530

    Made modifications to shitty code

--- sketch_sep02a/sketch_sep02a.ino
+++ sketch_sep02a/sketch_sep02a.ino
@@ -1,38 +1,63 @@
-
+//Erros for PID
 int prevError = 0;
 int currError;
+
+//P I D
 float P = 0;
 float I = 0;
 float D = 0;
+
+//PID stuff again
 int currPos;
 float correction;
+
+//PID constants
 const float Kp = 0;
 const float Kd = 0;
 const float Ki = 0;
+
+//The normal speed is 255, we are taking normalSpeed instant
 const int normalSpeed = 133;
+
+//speeds for 2 motors
 int speed1;
 int speed2;
-int M1 = 10;
-int M2 = 11;
-const int targetPos = 4;
-int Mm1 = 9;
-int Mm2 = 12;
-//end global variable declaration
 
+// connected to motor driver
+int MH1 = A0;
+int MH2 = A0;
+int ML1 = A2;
+int ML2 = A3;
+int E1 = 9;
+int E2 = 10;
+
+//we get values from 7 IR sensor pairs into values
+int values[6];
+int count;
+
+//target for PID
+const int targetPos = 4;
+
+
+//Setup for arduino
 void setup()
-{
+{       //setup input IR sensor pairs.
         for(int i=2;i<9;i++)
                 {
                         pinMode(i, INPUT);
                 }
-        pinMode(M1, OUTPUT);
-        pinMode(M2, OUTPUT);
-        pinMode(Mm1, OUTPUT);
-        pinMode(Mm2, OUTPUT);
-
-        digitalWrite(Mm1, LOW);
-        digitalWrite(Mm2, LOW);
-
+        //setup motor driver pins
+        pinMode(E1, OUTPUT);
+        pinMode(E2, OUTPUT);
+        pinMode(MH1, OUTPUT);
+        pinMode(MH2, OUTPUT);
+        pinMode(ML1, OUTPUT);
+        pinMode(ML2, OUTPUT);
+        //keep one end of each motor HIGH, and other low. (through motor driver)
+        digitalWrite(ML1, LOW);
+        digitalWrite(ML1, LOW);
+        digitalWrite(MH1, HIGH);
+        digitalWrite(MH2, HIGH);
 }
 
 void loop()
@@ -41,18 +66,21 @@ void loop()
         correction = calcPID(currPos);
         speed1 = normalSpeed - correction;
         speed2 = normalSpeed + correction;
-        analogWrite(M1, speed1);
-        analogWrite(M2, speed2);
+        analogWrite(E1, speed1);
+        analogWrite(E2, speed2);
 }
 
 int getCurrentPos()
 {
         for(int i=2;i<9;i++)
-                {
-                        int j = digitalRead(i);
-                        if(j==HIGH)
-                                return i;
-                 }
+                {       
+                        values[i-2] = digitalRead(i);
+                        if(values[i-2] == HIGH)
+                            count++;
+                }
+        int j;
+        j = returnInput(values, count);
+        return j;
 }//getting the current position as an integer
 
 int calcPID(int c)
@@ -66,3 +94,22 @@ int calcPID(int c)
         int corr = P+I+D;
         return (int)corr;
 }//returns the correction in speed as an integer
+
+int returnInput(int a[], int count)
+{
+    if(count == 0)
+        return 4;
+    else if(count >= 5)
+        return 4;
+    else{
+        for(int i=0; i<4; i++)
+            if(a[i] == HIGH)
+                return i;
+        
+        for(int i=8; i>4; i--)
+            if(a[i] == HIGH)
+                return i;
+
+    }
+    return 4;
+}
